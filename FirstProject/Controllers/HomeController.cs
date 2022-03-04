14ddb6461e68d793214eb8c1a1656fdfe7102539
rgummadi <rameshgummadi@gmail.com>
2022-03-04 12:26:25 +0530

    commit

---
+++ FirstProject/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using FirstProject.Models;
+
+namespace FirstProject.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public ViewResult Index()
+    {
+        int hour = DateTime.Now.Hour;
+        string viewModel = hour < 12 ? "Good Morning" : "Good Afternoon";
+        return View("MyView",viewModel);
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
